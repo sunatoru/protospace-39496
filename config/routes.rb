c45651d3ef1ae7c9b6987377827aee02d0929f0e
tano <supright34@gmail.com>
2023-09-09 18:10:29 +0900

    プロトタイプ情報の編集機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # devise導入時にルーティングも追加される
   devise_for :users
   root to: "prototypes#index"
-  resources :prototypes, only: [:index, :show, :new, :create] 
+  resources :prototypes, only: [:index, :show, :new, :create, :edit, :update] 
 
 end
